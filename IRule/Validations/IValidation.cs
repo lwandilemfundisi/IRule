84538e1e2c348842bba0a8aec89f48abaea0f63e
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-04-11 21:20:52 +0200

    Added custom attributes

---
+++ IRule/Validations/IValidation.cs
@@ -0,0 +1,32 @@
+﻿using IRule.Notifications;
+using Microservice.Framework.Common;
+
+namespace IRule.Validations
+{
+    public interface IValidation
+    {
+        SeverityType Severity { get; set; }
+
+        SystemCulture Culture { get; set; }
+
+        object Context { get; set; }
+
+        object Owner { get; set; }
+
+        Task<Notification> Validate(CancellationToken cancellationToken);
+
+        bool IsPropertyRule { get; set; }
+
+        bool MustValidate();
+
+        bool CanExecuteParallel();
+
+        string Name { get; set; }
+
+        string PropertyName { get; set; }
+
+        string ReadOnlyMessage { get; set; }
+
+        string DisplayName { get; set; }
+    }
+}
