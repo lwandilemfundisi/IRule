a635827c94f741714362c09dd9d20ae1a71d8d21
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-04-11 23:04:46 +0200

    save

---
+++ IRule.Tests/Scenarios/Required/RequiredValidationScenarioTests.cs
@@ -0,0 +1,51 @@
+﻿using IRule.Tests.Scenarios.TestScenarioRules;
+using IRule.Validations;
+using Microservice.Framework.Common;
+using Microsoft.Extensions.DependencyInjection;
+using IRule.Extensions;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace IRule.Tests.Scenarios.Required
+{
+    public class RequiredValidationScenarioTests
+    {
+        private IServiceProvider _serviceProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+
+            //Should this be Singleton or Transient?
+            services.TryAddTransient<IValidator, Validator>();
+
+            var ruleTypes = GetType().Assembly
+                .GetTypes()
+                .Where(t => typeof(IValidation).IsAssignableFrom(t) && !t.IsAbstract && !t.IsInterface);
+
+            foreach (var ruleType in ruleTypes)
+            {
+                services.TryAddTransient(ruleType);
+            }
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Test]
+        public async Task RuleTest()
+        {
+            var validator = _serviceProvider
+                .GetService<IValidator>();
+
+            var validationResult = await validator
+                .Validate(
+                new TestObject() { },
+                new TestObjectContext(),
+                SystemCulture.Default(),
+                this.GetType().Assembly,
+                CancellationToken.None);
+
+            Assert.IsTrue(validationResult.HasErrors());
+        }
+    }
+}
---
+++ IRule.Tests/Scenarios/TestScenarioRules/TestScenarios.cs
@@ -0,0 +1,24 @@
+﻿using IRule.Validations;
+using IRule.Validations.Attributes;
+using IRule.Validations.Common;
+
+namespace IRule.Tests.Scenarios.TestScenarioRules
+{
+    public class TestObject
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+
+    public class TestObjectContext
+    {
+    }
+
+    [Validation(typeof(TestObject))]
+    [ValidationProperty("Name")]
+    [ValidationProperty("Age")]
+    [ValidationContext(typeof(TestObjectContext))]
+    public class TestObjectRequiredValidation : RequiredValidation<TestObject>
+    {
+    }
+}
