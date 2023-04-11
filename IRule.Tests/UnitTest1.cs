adecb93ac99c3b0acfda6a274119094900556116
Lwandile Mfundisi <lwandilemf@tihsa.co.za>
2023-04-11 20:32:05 +0200

    Add project files.

---
+++ IRule.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace IRule.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
